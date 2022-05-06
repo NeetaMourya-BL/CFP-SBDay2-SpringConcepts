3c20b62e62fa0b1359097ee9faa36d3f2b4c071f
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-05-06 15:11:14 +0530

    Added: Create a Project to learn Spring Concepts

---
+++ src/main/java/com/example/sprinconcepts/SpringConceptsApplication.java
@@ -0,0 +1,21 @@
+package com.example.sprinconcepts;
+
+import com.example.sprinconcepts.component.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringConceptsApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Spring Concept Demo");
+		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+		System.out.println("Checking Context: " +context.getBean(DemoBean.class));
+	
+	}
+
+}
---
+++ src/main/java/com/example/sprinconcepts/component/DemoBean.java
@@ -0,0 +1,10 @@
+package com.example.sprinconcepts.component;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DemoBean {
+    public DemoBean(){
+        
+    }
+}
